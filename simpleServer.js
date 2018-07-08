4b09e20c0f091b7391df649888d4984c9b9e891d
Vinod Krishnan <vtkrishn@gmail.com>
2018-07-07 20:23:22 -0700

    comments for simple server

--- simpleServer.js
+++ simpleServer.js
@@ -1,8 +1,13 @@
+//variable for the http module
 var http = require('http');
 
+//create the server taking the function
 http.createServer(
+	//function with request and response
 	function (req,res){
+		//set the response write type
 		res.writeHead(200, {'Content-Type' : 'text/plain'});
+		//print this to the web page
 		res.end('Hello World');
 	}
-).listen(8080);
+).listen(8080); //listening port
