53ce26e771f154f0ba650bda37fdcf51b70f4297
Vinod Krishnan <vtkrishn@gmail.com>
2018-07-08 22:44:10 -0700

    process examples

--- globalObj.js
+++ globalObj.js
@@ -11,8 +11,38 @@ var t = setTimeout(() => {
 clearTimeout(t);
 
 //call every 2 seconds
-var callFunction = function () {
-	console.log('call function');
+//var callFunction = function () {
+//	console.log('call function');
+//}
+//var loop = setInterval(callFunction, 2000);
+
+//console
+console.info('Program started - Console');
+
+var variable = 10;
+console.log('simple some variable ' , variable);
+
+console.time('Data ');
+while(variable > 0){
+   variable--;
+   console.log('variable value :: '+ variable);
 }
-var loop = setInterval(callFunction, 2000);
+console.timeEnd('Data ');
+
+//process
+process.on('exit', (code) => {
+	setTimeout(()=>{
+		console.log('This will not return');
+	});
+	console.log('Exiting the program');
+});
+
+process.stdout.write('stdout.write \n');
+process.argv.forEach((val)=>{
+	console.log(val);
+});
+
+console.log(process.memoryUsage());
+
+
 
